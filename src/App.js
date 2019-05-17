9d8ab03dd284243a80b7c388ae65728d2a75ff63
Antonijo <antonijodev@gmail.com>
2019-05-17 14:13:30 +0100

    Added hardcoded to-do list

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li><input type="checkbox" />
+          <span>Bread</span>
+        </li>
+        <li><input type="checkbox" />
+          <span>Milk</span>
+        </li>
+        <li><input type="checkbox" />
+          <span>Egg</span>
+        </li>
+        <li><input type="checkbox" />
+          <span>Fruit</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
