5f5779d8fd110cd36e5ace6b7ab626884f2d1066
Antonijo <antonijodev@gmail.com>
2019-05-20 16:07:02 +0100

    Added styling for completed todos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './style.css';
 
 const TodoItem = (props) => {
+
+  const completedTodo = {
+    fontStyle: "italic",
+    color: '#cdcdcd',
+    textDecoration: "line-through"
+  }
+
   return (
     <div>
       <li className="ListItem">
@@ -11,7 +18,11 @@ const TodoItem = (props) => {
           checked={props.todo.completed}
           onChange={() => props.handleChange(props.todo.id)}
         />
-          <span className="Item">{props.todo.text}</span>
+        <span
+          className="Item"
+          style={props.todo.completed ? completedTodo : null}>
+          {props.todo.text}
+        </span>
       </li>
     </div>
   );
