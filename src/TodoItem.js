abb387ab9fa9c971ceef9fe602dcf4477ac5cec5
Antonijo <antonijodev@gmail.com>
2019-05-20 12:28:55 +0100

    Added event handler to checkbox input which is resolved to get rid of warning about not having an onChange event on input form

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,9 +4,15 @@ import './style.css';
 const TodoItem = (props) => {
   return (
     <div>
-        <li className="ListItem"><input type="checkbox" className="Checkbox" checked={props.todo.completed}/>
+      <li className="ListItem">
+        <input
+          type="checkbox"
+          className="Checkbox"
+          checked={props.todo.completed}
+          onChange={() => console.log("Changed!")}
+        />
           <span className="Item">{props.todo.text}</span>
-        </li>
+      </li>
     </div>
   );
 }
